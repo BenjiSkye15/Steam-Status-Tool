d5735bfd0f9181f52353248d4b8688d4ed23b29d
BenjiSkye15 <117398234+BenjiSkye15@users.noreply.github.com>
2024-05-23 19:01:14 -0400

    Code

---
+++ SST/SST/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Welcome to the Steam Status Tool (SST)!");
+Console.WriteLine("Everything is explained in the github repo (https://github.com/BenjiSkye15/Steam-Status-Tool).");
+Console.WriteLine("");
+Console.WriteLine("You can stop the program by pressing Enter.");
+Console.ReadLine();
+Console.Clear();
+Console.WriteLine("Exiting...");
+Thread.Sleep(3000);
